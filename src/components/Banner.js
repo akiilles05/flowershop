a13707adefc4a5d78aa2f67c9e8eb3e254e63afb
Illés Ákos <sellia0508@gmail.com>
2023-11-04 07:16:21 +0100

    Floweshop

---
+++ src/components/Banner.js
@@ -0,0 +1,92 @@
+import React from "react";
+
+import Image from "../assets/logo.png";
+
+import { TypeAnimation } from "react-type-animation";
+
+import { motion } from "framer-motion";
+
+import { fadeIn } from "../variants";
+
+import { Link } from "react-scroll";
+
+const Banner = () => {
+  return (
+    <section
+      className="min-h-[85vh] lg:min-h-[78vh] flex items-center"
+      id="home"
+    >
+      <div className="container mx-auto">
+        <div className="flex flex-col gap-y-8 lg:flex-row lg:items-center lg:gap-x-12">
+          <div className="flex-1 text-center font-secondary lg:text-left">
+            <motion.h1
+              variants={fadeIn("up", 0.3)}
+              initial="hidden"
+              whileInView={"show"}
+              viewport={{ once: false, amount: 0.7 }}
+              className="text-[55px] font-bold leading-[0.8] lg:text-[110px]"
+            >
+              Flower <span>Shop</span>
+            </motion.h1>
+
+            <motion.div
+              variants={fadeIn("up", 0.4)}
+              initial="hidden"
+              whileInView={"show"}
+              viewport={{ once: false, amount: 0.7 }}
+              className="mb-6 text-[36px] lg:text-[50px] font-secondary font-semibold uppercase leading-[1]"
+            >
+              <TypeAnimation
+                sequence={[
+                  "Flower bouquet",
+                  2000,
+                  "Wreath",
+                  2000,
+                  "Wedding Decoration",
+                  2000,
+                ]}
+                speed={50}
+                className="text-rose-300"
+                wrapper="span"
+                repeat={Infinity}
+              />
+              <span className="mr-4 text-white"> making</span>
+            </motion.div>
+            <motion.p
+              variants={fadeIn("up", 0.5)}
+              initial="hidden"
+              whileInView={"show"}
+              viewport={{ once: false, amount: 0.7 }}
+              className="mb-8 max-w-lg mx-auto lg:mx-0"
+            >
+              Flowers for every occasion!
+            </motion.p>
+            <motion.div
+              variants={fadeIn("up", 0.6)}
+              initial="hidden"
+              whileInView={"show"}
+              viewport={{ once: false, amount: 0.7 }}
+              className="flex max-w-max gap-x-6 items-center mb-12 mx-auto lg:mx-0"
+            >
+              <button className="btn btn-lg">Contact Me</button>
+              <a href="#" className="text-gradient btn-link ">
+                <Link to="services">Services</Link>
+              </a>
+            </motion.div>
+          </div>
+
+          <motion.div
+            variants={fadeIn("down", 0.5)}
+            initial="hidden"
+            whileInView={"show"}
+            className="hidden lg:flex flex-1 max-w-[320px] lg:max-w-[482px]"
+          >
+            <img src={Image} alt="" />
+          </motion.div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Banner;
